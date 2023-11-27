233715e3f8b9b33c9f8a5772d37b4eb2d34277ef
Muhammad Qasim Dilshad <qasimdilshad336@gmail.com>
2023-11-27 17:56:22 +0500

    title limit function & card button state added

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,20 +1,36 @@
 import React from "react";
 import Rating from "react-rating-stars-component";
+import { useState } from "react";
+
 
 function Card({ product }) {
- 
+
+  const [buttonText, setbuttonText] = useState("Add to Cart");
+
+function toggleButtonText() {
+     setbuttonText("View Cart");
+}
+const limitTitleWords = (title, limit) => {
+  const words = title.split(" ");
+
+  if (words.length > limit) {
+    return words.slice(0, limit).join(" ") + "...";
+  }
+
+  return title;
+};
 
   return (
     <div className="card">
       <div className="image">
         <img src={product.image} alt="Product" />
         <div className="overlay">
-          <button>Add to cart</button>
+          <button onClick={toggleButtonText}>{buttonText}</button>
         </div>
       </div>
       <div className="content">
         <div className="title">
-          <p>{product.title}</p>
+          <p>{limitTitleWords(product.title, 2)}</p>
         </div>
         <div className="price">
           <p>${product.price}</p>
