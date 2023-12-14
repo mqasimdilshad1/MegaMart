a63d2947952294627d5907ff6d1475504a6f083e
Muhammad Qasim Dilshad <qasimdilshad336@gmail.com>
2023-12-14 21:04:20 +0500

    card design updated

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -26,13 +26,13 @@ function Card({ product }) {
   return (
     // card
     <div
-      className="relative rounded-[16px] w-[14.1rem] h-[18rem] overflow-hidden box-border 
-   shadow-card-shadow border border-solid shadow-lg border-gray-300 group md:w-[14.1rem] md:h-[17.7rem] sm:w-[18.7rem] sm:h-[18.2rem]"
+      className="lg:relative rounded-[16px] lg:w-[14.1rem] lg:h-[18rem] overflow-hidden box-border 
+   shadow-card-shadow border border-solid shadow-lg border-gray-300 group md:w-[14.1rem] md:h-[17.7rem] w-[18.7rem] h-[18.2rem]"
     >
       {/* image */}
       <div className="w-[100%] h-[72%] overflow-hidden relative bg-white flex justify-center items-center      ">
         {/* sales-tag */}
-        <div className="absolute flex top-[-8px] right-[-13px] bg-primary text-white pt-[20px] pr-[33px] pb-[6px] pl-[10px] rounded-[10px] text-[14px] font-Inter z-[1] flex-col items-end justify-end sm:z-[1]">
+        <div className="absolute flex top-[-8px] right-[-13px] bg-primary text-white pt-[20px] pr-[33px] pb-[6px] pl-[10px] rounded-[10px] text-[14px] font-Inter flex-col items-end justify-end z-[1]">
           <span>{56}%</span>
           <span>OFF</span>
         </div>
@@ -42,27 +42,27 @@ function Card({ product }) {
           alt={product.title}
         />
         {/* card-overlay */}
-        <div className="absolute top-0 left-0 w-[100%] h-[100%] bg-overlay opacity-0 flex justify-center items-center z-[2] pointer-events-none group-hover:opacity-[1] sm:z-[2]"></div>
+        <div className="absolute top-0 left-0 w-[100%] h-[100%] bg-overlay opacity-0 flex justify-center items-center pointer-events-none group-hover:opacity-[1] z-[2]"></div>
       </div>
       {/* card content */}
       <div className="pl-[20px] font-Inter flex-col bg-searchbar-background ">
         {/* title price container */}
-        <div className="flex-col sm:flex-row sm:justify-around sm:mb-[10px] sm:mt-[5px] ">
+        <div className="lg:flex-col flex items-center justify-around mb-[10px] ">
           {/* title */}
           <div>
-            <p className="text-[1rem] font-Inter whitespace-nowrap overflow-hidden text-ellipsis max-w-[200px] sm:ml-[-30px] ">
+            <p className="text-[1rem] font-Inter whitespace-nowrap overflow-hidden text-ellipsis max-w-[150px] ml-[-30px] mt-2 ">
               {product.title}
             </p>
           </div>
           {/* price */}
           <div>
-            <p className="text-[0.9rem] text-heading mb-0 mt-[5px] font-bold sm:mt-[10px] ">
+            <p className="text-[0.9rem] text-heading mb-0 lg:mt-[5px] font-bold mt-[12px] ">
               ${product.price}
             </p>
           </div>
         </div>
         {/* rating button container */}
-        <div className="flex sm:gap-0 ">
+        <div className="flex gap-0 ">
           {/* rating */}
           <div className="mt-0">
             <Rating
@@ -75,9 +75,9 @@ function Card({ product }) {
             />
           </div>
           <button
-            className="bg-white pt-[6px] pr-[23px] pb-[6px] pl-[23px] rounded-[8px] border-none 
-         text-[0.9rem] font-Inter font-[500] outline-none absolute top-[30%] invisible z-[2] left-[21%]
-         w-[132px] group-hover:visible hover:bg-button-hover focus:flex-row focus:items-baseline sm:relative sm:visible sm:ml-[-30px] sm:bg-button-hover sm:rounded-[6px] sm:p-x-[20px] sm:p-y-[5px] sm:border-none "
+            className="lg:bg-white md:bg-white pt-[6px] pr-[23px] pb-[6px] pl-[23px] border-none 
+         text-[0.9rem] font-Inter font-[500] outline-none lg:absolute md:absolute top-[30%] lg:invisible md:invisible z-[2] left-[21%]
+         w-[132px] lg:group-hover:visible hover:bg-button-hover focus:flex-row focus:items-baseline relative visible ml-[-30px] bg-button-hover rounded-[6px] p-x-[20px] p-y-[5px] mb-1 "
             onClick={handleButtonClick}
           >
             {showCartIcon && <BsCart2 size={13} />}
